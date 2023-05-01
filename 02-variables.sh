d5b66f55402ea53c2a586167643630562e9205bc
CathLa <catherinelavoie301@gmail.com>
2023-05-01 10:56:38 -0400

    fonction

--- 02-variables.sh
+++ 02-variables.sh
@@ -7,6 +7,6 @@ echo $prenom
 declare -i age=33  #on force un type, mais utiliser très rarement (int)
 declare -r CHEMIN=/bin  #mettre en lecture seule ---CONSTANTE
 
-echo "Nom script" $0
+echo "Nom script" $0 #Donne le nom du scrip ou le nom de la fonction
 echo "Nombre params" $#
 echo "Perso 1 : " $1 #Ajouter de variable à la suite de la commande d'exécution du code
