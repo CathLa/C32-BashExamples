e102aadaa00b7e75bd5bf6da8f24bfb203c3e3ab
CathLa <catherinelavoie301@gmail.com>
2023-05-01 10:03:36 -0400

    calcul wow.

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+#pour déclarer une variable IL NE FAUT PAS METTRE D'ESPACE
+prenom=Neo #Si pas composé pas besoin de " "
+echo $prenom
+
+declare -i age=33  #on force un type, mais utiliser très rarement (int)
+declare -r CHEMIN=/bin  #mettre en lecture seule ---CONSTANTE
+
+echo "Nom script" $0
+echo "Nombre params" $#
+echo "Perso 1 : " $1 #Ajouter de variable à la suite de la commande d'exécution du code
--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+echo 10+10 #suite de caractère
+echo $((10+10)) #mystere, ca prend (()) pour faire le calcul
+valeur=$((10+10))
+echo $valeur
+
+read -p "nb1 : " nb1
+read -p "nb2 : " nb2
+let resultat=$nb1*$nb2
+echo $resultat 
