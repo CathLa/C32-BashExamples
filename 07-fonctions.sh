aefc4a9b3e009f94b50794a4eb72c4e8bf5c1329
CathLa <catherinelavoie301@gmail.com>
2023-05-01 10:59:11 -0400

    Expression réguliere

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+maFonction() {
+    echo "param : " $1 #$0 donne le nom de la fonction 
+}
+
+maFonction "salut" 
+maFonction "toi"
+#quand tu passes $1 dans ta fonction c'est les paramètres passer dans la fonction et pas dans la commande. 
+#si tu fais echo $1 en dehors de la fonction, va prendre le parametre passé lors de la commande
