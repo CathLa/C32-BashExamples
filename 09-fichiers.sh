478f3a1622379829dbf7e5e5ae1fb58b3ddfa3db
CathLa <catherinelavoie301@gmail.com>
2023-05-01 11:05:31 -0400

    fichier

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+ # -e = fichier existe ? 
+ # -f = c'est un fichier ? retourne un true false
+ # -d= c'est un dossier ? 
+
+
+if [[ -d $1 ]] #passer argument lors de l'appel de la commande. 
+then 
+    echo "Le fichier existe" 
+fi 
